68b0e5857a129d96d44d7d4fb2b97a44d9a992d3
Daaron <grundoeat@gmail.com>
2015-05-01 01:10:09 +1000

    solved cf 479c

--- codeforces/479c.cpp
+++ codeforces/479c.cpp
@@ -1,2 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+#define A first
+#define B second
+#define fo(x,y,z) for(int x=y;x<z;x++)
+
+int n;
+pair<int, int> a[5005];
+
+int main(){
+	scanf("%d", &n);
+	fo(i,0,n) scanf("%d %d", &a[i].A, &a[i].B);
+	sort(a, a+n);
+	int ans = -1;
+	fo(i,0,n) ans = (a[i].B >= ans)?a[i].B:a[i].A;
+	printf("%d\n", ans);
+}
+
